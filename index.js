ded080d785e66246057be503f762658e33bbcfbb
Vikas Kashyap <159269579+vikas-kashyap97@users.noreply.github.com>
2024-10-02 10:24:26 +0000

    commit

---
+++ index.js
@@ -0,0 +1,145 @@
+// console.log("Hello");
+// console.log(1+3);
+
+// // Conditional statement
+
+// let age = 18;
+// let name = "Vikas"
+// if (age >= 18) {
+//     if (name == "Vikas") {    
+//     }
+//     console.log("You are Eligible!");
+//     console.log("Your name is Vikas");
+    
+// } else {
+//     console.log("You are not Eligible!");
+
+// }
+
+// let a = 18;
+// let b = 18;
+
+// if (a > b ) {
+//     console.log("a is greater");
+// } else if(b>a){
+//     console.log("b is greater");
+// } else {
+//     console.log("Both are equal");
+// }
+
+// let ch=2;
+
+// switch (ch) {
+//     case 1:
+//         console.log("i am 1");
+//         break;
+//     case 2:
+//         console.log("i am 2");
+//         break;
+//     case 3:
+//         console.log("i am 3");
+//         break;
+//     default:
+//         console.log("I am a number");
+//         break;
+// }
+
+// // loops - for , while, do while
+
+// for (let i = 1; i <= 10 ; i++) {
+//     console.log(`2 * ${i} = ${2*i}`);
+// }
+
+// var i = 1;
+// while (i<=10) {
+//     console.log(`2 * ${i} = ${2*i}`);
+//     i++;
+// }
+
+// let i = 1
+// do {
+//     console.log(`2 * ${i} = ${2*i}`);
+//     i++;
+// } while (i<= 10);
+
+// var, let, const
+
+
+
+// let firstName = "Vikas";
+// let lastName = " Kashyap";
+// console.log(`${firstName}${lastName}`);
+
+// Datatype
+//  1. Primitive datatypes
+// let num = 50;
+// console.log(typeof num);
+
+// let str = "hello"
+// console.log(str);
+
+// let bool = true
+// console.log(typeof bool);
+
+// let a = null
+// console.log(typeof a);
+
+// let b
+// console.log(b);
+
+
+// result = 5;
+// result = String(result)
+// console.log(typeof result);
+
+// Operators 
+
+//airthmetic
+
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 5);
+// console.log(10 % 4);
+// console.log(10 ** 4);
+
+// let a = 1;
+// a++;
+// console.log(a);
+
+//Comparision
+
+// a="5";
+// b=6;
+// console.log(a===b);
+// console.log(a!==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+// Logical
+
+// console.log(4>3 || 3>4);
+// console.log(4>3 && 3>4);
+// console.log(!true);
+
+// [1, 2, 3].forEach(x => console.log(x));  // 1 2 3
+
+// const result = [3, 1, 2,6,8,2,3,5,4,956,4,4,8,8,56,9,54,8,5,5,6,4,5,9,6,55,4,].reverse();  // [1, 2, 3]
+// console.log(result);
+
+const arr = [1, 2, 3, 4];
+arr.copyWithin(1, 2);  // [1, 3, 4, 4]
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
